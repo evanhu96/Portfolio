bc014cd69ca8a2cc486f7c4871f8146b329100fe
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-10-02 17:28:30 -0600

    fixed app name

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -162,7 +162,7 @@ const quoteP = document.querySelector("p#quote");
 const bground = document.querySelector("body");
 
 const fontType = ["Roboto Mono", "Roboto Slab", "Abril Fatface", "Notable", "Bungee"];
-const colours = ["#FFCDD2", "#FCE4EC", "#F3E5F5", "#8C9EFF", "#90CAF9", "#80D8FF", "#80DEEA", "#B2DFDB", "#69F0AE", "#AED581", "#AED581", "#FFC400", "#BCAAA4", "#90A4AE"];
+const colors = ["#FFCDD2", "#FCE4EC", "#F3E5F5", "#8C9EFF", "#90CAF9", "#80D8FF", "#80DEEA", "#B2DFDB", "#69F0AE", "#AED581", "#AED581", "#FFC400", "#BCAAA4", "#90A4AE"];
 
 function getQuotes() {
     API.get(API_URL).then(data => addQuote(data['slip']['advice']))
@@ -171,9 +171,9 @@ function getQuotes() {
 function addQuote(quote) {
     quoteP.innerText = quote;
     let fontsNum = Math.floor(Math.random()*fontType.length);
-    let coloursNum = Math.floor(Math.random()*colours.length);
+    let colorsNum = Math.floor(Math.random()*colors.length);
     quoteP.style.fontFamily = fontType[fontsNum];
-    bground.style.backgroundColor = colours[coloursNum]}
+    bground.style.backgroundColor = colors[colorsNum]}
 
     const reloadButton = document.querySelector("button#reload")
 reloadButton.addEventListener("click", ()=> getQuotes());
