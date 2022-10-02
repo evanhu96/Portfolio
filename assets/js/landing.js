80cb24631d57f5ff7c57c07845e5e32db5ffba81
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-10-02 17:16:07 -0600

    added Advice API

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -153,3 +153,29 @@ function hide() {
 
 //     player.connect();
 // };
+
+const API_URL = "https://api.adviceslip.com/advice";
+function get(url) {return fetch(url).then(resp => resp.json())};
+const API = { get };
+
+const quoteP = document.querySelector("p#quote");
+const bground = document.querySelector("body");
+
+const fontType = ["Roboto Mono", "Roboto Slab", "Abril Fatface", "Notable", "Bungee"];
+const colours = ["#FFCDD2", "#FCE4EC", "#F3E5F5", "#8C9EFF", "#90CAF9", "#80D8FF", "#80DEEA", "#B2DFDB", "#69F0AE", "#AED581", "#AED581", "#FFC400", "#BCAAA4", "#90A4AE"];
+
+function getQuotes() {
+    API.get(API_URL).then(data => addQuote(data['slip']['advice']))
+};
+
+function addQuote(quote) {
+    quoteP.innerText = quote;
+    let fontsNum = Math.floor(Math.random()*fontType.length);
+    let coloursNum = Math.floor(Math.random()*colours.length);
+    quoteP.style.fontFamily = fontType[fontsNum];
+    bground.style.backgroundColor = colours[coloursNum]}
+
+    const reloadButton = document.querySelector("button#reload")
+reloadButton.addEventListener("click", ()=> getQuotes());
+
+document.body.onload = getQuotes;
